bfc318fca6ebef7d97db27ee4093a0dacb9c6e6c
Peter Klapwijk <38850821+PeterKlapwijk@users.noreply.github.com>
2022-06-25 09:31:17 +0200

    Update LicenseDetect.ps1

--- ActivateEmbeddedProductKey/LicenseDetect.ps1
+++ ActivateEmbeddedProductKey/LicenseDetect.ps1
@@ -1,12 +1,12 @@
 # --------------------------------------------------------------------------------------------- # 
-# Author(s)    : Peter Klapwijk - www.InTheCloud247.com      					                #
+# Author(s)    : Peter Klapwijk - www.InTheCloud247.com      				        #
 # Version      : 1.0                                                                            #
 #                                                                                               #
 # Description  : Detection script to be used with Microsoft Intune, determines if the current   #
-#                Product Key Channel is OEM or Retail								            #
-#                														                        #
+#                Product Key Channel is OEM or Retail					        #
+#                										#
 # Changes      : v1.0 Initial version	                                                        #
-#                														                        #
+#                										#
 # --------------------------------------------------------------------------------------------- #
 
 $getreg=Get-WmiObject SoftwareLicensingProduct -Filter "ApplicationID = '55c92734-d682-4d71-983e-d6ec3f16059f' and LicenseStatus = '1'"
